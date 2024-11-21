038a7c7807d99e052525876db9fa73910fd74a88
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2024-11-21 02:41:32 +0100

    good results!

--- pottscpp/main/teste.cpp
+++ pottscpp/main/teste.cpp
@@ -4,19 +4,19 @@
 
 int main()
 {
-    int L = 15;
+    int L = 20;
     float J = 1.0;
     int q = 8;
-    double f_tol = 1.0e-8, h_tol = 0.85;
+    double f_tol = 1.0e-6, h_tol = 0.80;
     bool NoLog = false;
-    int sampleInterval = 75000, f_factor = 2;
-    std::string filename = "L15_J1_q8.txt";
+    int sampleInterval = 5000, f_factor = 5;
+    std::string filename = "L20_J1_q8.txt";
 
     PottsLattice potts(L, q, J);
     potts.PrintLattice();
     std::cout << "Energy = " << potts.Potts_Energy() << std::endl;
 
-    std::map<int, double> lng = WangLandauPotts(potts, 100000, q, f_tol, h_tol, NoLog, sampleInterval, f_factor, filename);
+    std::map<int, double> lng = WangLandauPotts(potts, 10000, q, f_tol, h_tol, NoLog, sampleInterval, f_factor, filename);
     save_data(lng, "lng.txt");
     return 0;
 }
