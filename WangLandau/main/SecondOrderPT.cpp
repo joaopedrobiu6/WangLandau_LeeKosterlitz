a83d25a3945f2413a3f81de13fdfa961860350ba
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2024-11-29 15:41:29 +0100

    results obtained

--- WangLandau/main/SecondOrderPT.cpp
+++ WangLandau/main/SecondOrderPT.cpp
@@ -6,16 +6,16 @@ int main()
 {
     int L = 12;
     float J = 1.0;
-    int q = 3;
-    double f_tol = 1.0e-6, h_tol = 0.80;
+    int q = 5;
+    double f_tol = 1.0e-8, h_tol = 0.80;
     bool NoLog = false;
-    int sampleInterval = 5000, f_factor = 5;
+    int sampleInterval = 10000, f_factor = 2;
     std::string filename = GetFilename(L, J, q);
 
     PottsLattice potts(L, q, J);
     potts.PrintLattice();
     std::cout << "Energy = " << potts.Potts_Energy() << std::endl;
 
-    std::map<int, double> lng = WangLandauPotts(potts, 10000, q, f_tol, h_tol, NoLog, sampleInterval, f_factor, filename);
+    std::map<int, double> lng = WangLandauPotts(potts, 10000000, q, f_tol, h_tol, NoLog, sampleInterval, f_factor, filename);
     return 0;
 }
--- WangLandau/main/teste.cpp
+++ WangLandau/main/teste.cpp
@@ -4,19 +4,18 @@
 
 int main()
 {
-    int L = 20;
+    int L = 12;
     float J = 1.0;
     int q = 8;
-    double f_tol = 1.0e-6, h_tol = 0.80;
+    double f_tol = 1.0e-8, h_tol = 0.95;
     bool NoLog = false;
-    int sampleInterval = 5000, f_factor = 5;
-    std::string filename = "L20_J1_q8.txt";
+    int sampleInterval = 2000, f_factor = 2;
+    std::string filename = GetFilename(L, J, q);
 
     PottsLattice potts(L, q, J);
     potts.PrintLattice();
     std::cout << "Energy = " << potts.Potts_Energy() << std::endl;
 
     std::map<int, double> lng = WangLandauPotts(potts, 10000, q, f_tol, h_tol, NoLog, sampleInterval, f_factor, filename);
-    save_data(lng, "lng.txt");
     return 0;
 }
